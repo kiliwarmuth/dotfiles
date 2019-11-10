66794b08cc738fa8f577b2a3b6518fbd171b8b8f
089kili <kilian.warmuth@icloud.com>
2019-11-10 20:13:43 +0100

    updated colors

--- zsh/updconfig.sh
+++ zsh/updconfig.sh
@@ -15,5 +15,8 @@ cp -ru ~/.config/neofetch/config.conf ~/Dokumente/Github/dotfiles/neofetch
 cp -ru /etc/X11/xorg.conf.d/90-touchpad.conf ~/Dokumente/Github/dotfiles/sonstiges
 cp -ru ~/.config/spicetify/* ~/Dokumente/Github/dotfiles/spicetify
 cp -ru ~/.mozilla/firefox/uri41cyr.default-release/chrome/* ~/Dokumente/Github/dotfiles/firefox/chrome
+cp -ru ~/.themes/Custom-Dark ~/Dokumente/Github/dotfiles/themes
+cp -ru ~/.icons/capitaine-cursors ~/Dokumente/Github/dotfiles/themes
+cp -ru ~/.icons/korla ~/Dokumente/Github/dotfiles/themes
 cp -ru ~/Programme/scripts/updconfig.sh ~/Dokumente/Github/dotfiles/zsh
 
