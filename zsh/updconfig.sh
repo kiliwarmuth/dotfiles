83c7b3945d0c6f0c79b6c7faaf3c2315237aeece
089kili <kilian.warmuth@icloud.com>
2020-02-23 01:31:32 +0100

    updated

--- zsh/updconfig.sh
+++ zsh/updconfig.sh
@@ -1,28 +1,28 @@
 # copy config files
 
-cp -ru ~/.config/i3/* ~/Dokumente/Github/dotfiles/i3
-cp -ru ~/.config/polybar/* ~/Dokumente/Github/dotfiles/polybar
-cp -ru ~/.config/ranger/rc.conf ~/Dokumente/Github/dotfiles/bin/ranger
-cp -ru ~/.config/ranger/rifle.conf ~/Dokumente/Github/dotfiles/bin/ranger
-cp -ru ~/.config/ranger/scope.sh ~/Dokumente/Github/dotfiles/bin/ranger
-cp -ru ~/.config/rofi/config.rasi ~/Dokumente/Github/dotfiles/rofi
-cp -ru ~/.config/zathura/zathurarc ~/Dokumente/Github/dotfiles/zathura
-cp -ru ~/.Xresources ~/Dokumente/Github/dotfiles/Xresources
-cp -ru ~/.vimrc ~/Dokumente/Github/dotfiles/bin/vim
-cp -ru ~/.zshrc ~/Dokumente/Github/dotfiles/zsh
-cp -ru ~/.xinitrc ~/Dokumente/Github/dotfiles/bin/other_stuff/.xinitrc
-cp -ru ~/.zprofile ~/Dokumente/Github/dotfiles/bin/other_stuff/.zprofile
-cp -ru ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/Dokumente/Github/dotfiles/zsh
-cp -ru ~/.config/Code\ -\ OSS/User/settings.json ~/Dokumente/Github/dotfiles/bin/vscode
-cp -ru ~/.config/neofetch/config.conf ~/Dokumente/Github/dotfiles/bin/neofetch
-cp -ru /etc/X11/xorg.conf.d/90-touchpad.conf ~/Dokumente/Github/dotfiles/bin/other_stuff
-cp -ru ~/.config/spicetify/* ~/Dokumente/Github/dotfiles/bin/spicetify
-cp -ru ~/.config/cava/config ~/Dokumente/Github/dotfiles/bin/cava
-cp -ru ~/.mozilla/firefox/uri41cyr.default-release/chrome/* ~/Dokumente/Github/dotfiles/firefox/chrome
-cp -ru ~/.themes/Custom-Dark ~/Dokumente/Github/dotfiles/themes
-cp -ru ~/Programme/scripts/updconfig.sh ~/Dokumente/Github/dotfiles/zsh
-cp -ru /etc/pacman.d/hooks/* ~/Dokumente/Github/dotfiles/bin/pacman
-cp -ru ~/.config/compton/compton.conf ~/Dokumente/Github/dotfiles/compton
+cp -ruv ~/.config/i3/* ~/Dokumente/Github/dotfiles/i3
+cp -ruv ~/.config/polybar/* ~/Dokumente/Github/dotfiles/polybar
+cp -ruv ~/.config/ranger/rc.conf ~/Dokumente/Github/dotfiles/bin/ranger
+cp -ruv ~/.config/ranger/rifle.conf ~/Dokumente/Github/dotfiles/bin/ranger
+cp -ruv ~/.config/ranger/scope.sh ~/Dokumente/Github/dotfiles/bin/ranger
+cp -ruv ~/.config/rofi/config.rasi ~/Dokumente/Github/dotfiles/rofi
+cp -ruv ~/.config/zathura/zathurarc ~/Dokumente/Github/dotfiles/zathura
+cp -ruv ~/.Xresources ~/Dokumente/Github/dotfiles/Xresources
+cp -ruv ~/.vimrc ~/Dokumente/Github/dotfiles/bin/vim
+cp -ruv ~/.zshrc ~/Dokumente/Github/dotfiles/zsh
+cp -ruv ~/.xinitrc ~/Dokumente/Github/dotfiles/bin/other_stuff/.xinitrc
+cp -ruv ~/.zprofile ~/Dokumente/Github/dotfiles/bin/other_stuff/.zprofile
+cp -ruv ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/Dokumente/Github/dotfiles/zsh
+cp -ruv ~/.config/Code\ -\ OSS/User/settings.json ~/Dokumente/Github/dotfiles/bin/vscode
+cp -ruv ~/.config/neofetch/config.conf ~/Dokumente/Github/dotfiles/bin/neofetch
+cp -ruv /etc/X11/xorg.conf.d/90-touchpad.conf ~/Dokumente/Github/dotfiles/bin/other_stuff
+cp -ruv ~/.config/spicetify/* ~/Dokumente/Github/dotfiles/bin/spicetify
+cp -ruv ~/.config/cava/config ~/Dokumente/Github/dotfiles/bin/cava
+cp -ruv ~/.mozilla/firefox/uri41cyr.default-release/chrome/* ~/Dokumente/Github/dotfiles/firefox/chrome
+cp -ruv ~/.themes/Custom-Dark ~/Dokumente/Github/dotfiles/themes
+cp -ruv ~/Programme/scripts/updconfig.sh ~/Dokumente/Github/dotfiles/zsh
+cp -ruv /etc/pacman.d/hooks/* ~/Dokumente/Github/dotfiles/bin/pacman
+cp -ruv ~/.config/compton/compton.conf ~/Dokumente/Github/dotfiles/compton
 
 # update list of installed packages
 pacman -Qqe > ~/Dokumente/Github/dotfiles/bin/pacman/pkglist.txt
