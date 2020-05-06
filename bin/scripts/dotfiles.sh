862adffca0a60aec9a24cd4b3546348278e0a489
089kili <kilian.warmuth@icloud.com>
2020-05-06 17:52:27 +0200

    updated

--- bin/scripts/dotfiles.sh
+++ bin/scripts/dotfiles.sh
@@ -37,6 +37,8 @@ cp -ru ~/.mozilla/firefox/uri41cyr.default-release/chrome/* ~/Dokumente/Github/d
 cp -ru ~/.themes/Custom-Dark/* ~/Dokumente/Github/dotfiles/themes/Custom-Dark-GTK
 # compton
 cp -ru ~/.config/compton/compton.conf ~/Dokumente/Github/dotfiles/config/compton
+# grub
+cp -ru /etc/default/grub ~/Dokumente/Github/dotfiles/other_stuff/grub
 # script
 cp -ru ~/.bin/scripts/dotfiles.sh ~/Dokumente/Github/dotfiles/bin/scripts
 cp -ru ~/.bin/scripts/spotify_status.py ~/Dokumente/Github/dotfiles/bin/scripts
